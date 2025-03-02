52c13af9f6711c10daea7f32157aaa5199667ab6
Chuck Boecking <chuck@chuboe.com>
2025-03-02 12:03:14 -0600

    stage -05

--- util/stage.sh
+++ util/stage.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-source $WI_REPO_DIR/config.properties
+# fully qualified script path and name
+SC_SCRIPT_DIR_NAME=$(readlink -f "$0")
+echo SC_SCRIPT_DIR_NAME=$SC_SCRIPT_DIR_NAME
+# fully qualified script path
+SC_SCRIPT_DIR=$(dirname "$SC_SCRIPT_DIR_NAME")
+echo SC_SCRIPT_DIR=$SC_SCRIPT_DIR
+
+cd $SC_SCRIPT_DIR
+
+source ../config.properties
 
 sudo mkdir -p $WI_REPO_DIR/rag-stage
 sudo cp $WI_SRC_DIR/*.md $WI_REPO_DIR/rag-stage/
