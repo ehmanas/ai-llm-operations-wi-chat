5656ecfce7d23dc7b797fc23f72906de3ce038ed
Chuck Boecking <chuck@chuboe.com>
2025-03-17 17:55:55 -0500

    added arguments

--- util/ai-launcher.sh
+++ util/ai-launcher.sh
@@ -7,5 +7,9 @@ NEW_DIR=/home/CHAT_USER/.aichat-history/aichat-$CHAT_DATE
 mkdir -p /home/CHAT_USER/.aichat-history/
 cp -r /home/CHAT_USER/.config/aichat/ $NEW_DIR
 
+# $1 is the argument passed into ttyd and ultimately this script
+echo arg=$1 >> /tmp/out.txt
+export STK_ARG1=$1
+
 export AICHAT_CONFIG_DIR="$NEW_DIR"
 /usr/local/bin/aichat --rag AI_RAG_ALL --role AI_ROLE_STARTER
